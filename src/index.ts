6ba673ed99c5f554872ee7a9bcead2f541af2b18
kabiaa <kailum.a.bull@gmail.com>
2021-12-20 16:19:33 +0800

    Import Contexts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import Registrar from './registrar'
 import Module from './module'
 import { Store as VuexStore } from 'vuex'
+import { ActionContext, GetterContext } from './declarations'
 
 export {
   Module,
-  Registrar
+  Registrar,
+  ActionContext,
+  GetterContext
 }
 
 export default {
